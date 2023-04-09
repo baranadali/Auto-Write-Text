193e1d6a542b06eaa8b1faa5d402a975ee511b6e
Praveensenpai <71966071+Praveensenpai@users.noreply.github.com>
2023-04-09 17:41:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
 const awText = "An automated post is being written here and it's very exciting to print it.";
-    let index = 0;
-    let timer = 150; //Time of writing..
+let index = 0;
+const timer = 150; // Time of writing in milliseconds.
 
+function writeText() {
+    document.body.innerText = awText.slice(0, ++index % awText.length);
+}
 
-    function writeText() {
-        document.body.innerText = awText.slice(0, index);
-        index++;
-        
-        if (index > awText.length -1) {
-            index = 0;
-        }
-    };
-    setInterval(writeText, timer);
+setInterval(writeText, timer);
